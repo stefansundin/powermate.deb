4998cdccf74e9912c463948e1266c595d9317053
Stefan Sundin <git@stefansundin.com>
2018-12-19 21:46:38 -0800

    Update tooling.

--- generate.sh
+++ generate.sh
@@ -1,6 +1,2 @@
-DIR=$(dirname "$0")
-ppastats stefansundin powermate -o "$DIR"
-git -C "$DIR" add .
-git -C "$DIR" commit -am "`date +%F`"
-git -C "$DIR" push
-
+#!/bin/bash -e
+ppastats stefansundin powermate -o .
